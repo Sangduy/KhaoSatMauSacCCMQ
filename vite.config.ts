95fab1b520ccc4ad4904c3836ae638a240f6fcd6
Sangduy <60996946+Sangduy@users.noreply.github.com>
2026-01-08 22:54:13 +0700

    Change base path for Electron asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Quan trọng: Để Electron load được file asset (css, js) từ ổ cứng
+  base: '/KhaoSatMauSacCCMQ/', // Quan trọng: Để Electron load được file asset (css, js) từ ổ cứng
 });
